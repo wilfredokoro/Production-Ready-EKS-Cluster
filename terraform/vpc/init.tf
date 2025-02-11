9d958f07f1c07d18600b6ac7caeae8ec974b5c87
wilfredokoro <101127016+wilfredokoro@users.noreply.github.com>
2025-02-11 08:27:28 -0600

    edit workflow

--- terraform/vpc/init.tf
+++ terraform/vpc/init.tf
@@ -1,12 +1,11 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-bobo12345"
-    key    = "vpc/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "terraform-state-bobo12345"
+    key            = "vpc/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "fred-terraform-state01"
+    role_arn       = "arn:aws:iam::887998956998:role/GitHubActionsRole"
 
-    assume_role {
-      role_arn = "arn:aws:iam::887998956998:role/GitHubActionsRole"
-    }
   }
 }
 
